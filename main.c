807b5d6c46c2be07bd7ad3d6267baab58e5279c6
Woxel <143615249+woxels@users.noreply.github.com>
2023-09-01 00:09:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -869,6 +869,10 @@ int main(int argc, char** argv)
     printf("2nd, \"0.003\", Mouse sensitivity.\n");
     printf("3rd, \"/tmp/colors.txt\", path to a color palette file, the file must contain a hex\n");
     printf("color on each new line, 32 colors maximum. e.g; \"#00FFFF\".\n\n");
+    printf("To load from file: ./wox load <file_path>\n");
+    printf("e.g; ./wox load /home/user/file.wox.gz\n\n");
+    printf("To export: ./wox export <project_name> <option: wox,csv,ogl,ply> <export_path>\n");
+    printf("e.g; ./wox export ply /home/user/file.ply\n\n");
     printf("Find more color palettes at; https://lospec.com/palette-list\n");
     printf("You can use any palette upto 32 colors. But don't use #000000 (Black)\nin your color palette as it will terminate at that color.\n\n");
     printf("Default 32 Color Palette: https://lospec.com/palette-list/resurrect-32\n");
