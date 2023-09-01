cf34d27c2d9d2ea294ad2d52a03afb19fd174fcf
Woxel <143615249+woxels@users.noreply.github.com>
2023-09-01 12:32:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -992,13 +992,13 @@ int main(int argc, char** argv)
                 fprintf(f, "# X Y Z RRGGBB\n");
                 for(uint i = 0; i < max_voxels; i++)
                 {
-                    if(g.voxels[i] == 0){continue;}
+                    if(g.voxels[i] < 8){continue;}
                     const vec p = ITP(i);
-                    const uint tu = g.colors[g.voxels[i]];
+                    const uint tu = g.colors[g.voxels[i]-1];
                     uchar r = (tu & 0x00FF0000) >> 16;
                     uchar gc = (tu & 0x0000FF00) >> 8;
                     uchar b = (tu & 0x000000FF);
-                    if(g.colors[g.voxels[i]-1] != 0)
+                    if(r != 0 && gc != 0 && b != 0)
                         fprintf(f, "%g %g %g %02X%02X%02X\n", p.x-64.f,p.y-64.f,p.z, r,gc,b);
                 }
                 fclose(f);
